630c04062641d0a86be62df4a9804dea85e23364
alex-romanov <alejandro@mintitmedia.com>
2020-03-06 11:39:21 -0800

    save point: lesson 17

---
+++ sick-fits/frontend/components/CreateItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+
+class CreateItem extends Component {
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default CreateItem;
